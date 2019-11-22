728a757b1e65126115815d995bb8c9bb904eeab8
Cuselmusel <benni_herzog@msn.com>
2019-11-22 11:39:04 +0100

    commit on 22/11/2019 at 11:39:04

--- firstScript.sh
+++ firstScript.sh
@@ -1,6 +1,6 @@
 touch example1.txt
 touch example2.txt
 touch example3.txt
-ls -a >> example1.txt
 cat example1.txt
-sort example1.txt | tail -n1 
+ls -a >> example1.txt
+sort example1.txt | tail -n1 > example2.txt
